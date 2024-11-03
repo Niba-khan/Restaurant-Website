83e95b4f17ec483e9cd3d9293b8c8fe75893930e
Niba farooq <164494729+Niba-khan@users.noreply.github.com>
2024-11-03 15:11:26 +0500

    Website

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images:{
+        domains:[ 'media.lordicon.com', 'img95.lovepik.com', "i.gifer.com", 'encrypted-tbn0.gstatic.com', 'cdn.trendhunterstatic.com', 'www.tohfay.com','images.deliveryhero.io', 'magazine.foodpanda.pk', 'combo1.avif' ]
+    }
+};
+
+export default nextConfig;
